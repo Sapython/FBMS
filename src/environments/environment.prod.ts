cc0461ea2e3b263a183dc6d4feac65250208c079
Saptam <saptampro2003@gmail.com>
2022-07-27 16:24:32 +0530

    Added firebase and PWA

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'fbms-shreeva-demo',
+    appId: '1:403464137223:web:9a9e4c34c95bc766a858a2',
+    storageBucket: 'fbms-shreeva-demo.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyATbn4lcWC9FwXTIHpA9iVyeADvdExh-bo',
+    authDomain: 'fbms-shreeva-demo.firebaseapp.com',
+    messagingSenderId: '403464137223',
+    measurementId: 'G-T77N187VLZ',
+  },
   production: true
 };
