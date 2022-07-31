16192ef9aeb57373e8ff5c89484be4ed92ec6351
Saptam <saptampro2003@gmail.com>
2022-07-31 16:01:37 +0530

    Added dish card.

--- src/app/components/menu-dish-card/menu-dish-card.component.ts
+++ src/app/components/menu-dish-card/menu-dish-card.component.ts
@@ -1,12 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 @Component({
   selector: 'app-menu-dish-card',
   templateUrl: './menu-dish-card.component.html',
-  styleUrls: ['./menu-dish-card.component.scss']
+  styleUrls: ['./menu-dish-card.component.scss'], 
 })
 export class MenuDishCardComponent implements OnInit {
-
+  @Input() imageSource:string = './assets/images/dish.png';
+  @Input() dishName:string = "Simple Pasta";
+  @Input() dishPrice:number = 100;
+  @Input() servesLeft:number = 15; 
   constructor() { }
 
   ngOnInit(): void {
