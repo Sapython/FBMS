a6b9743d9f2bbf2453ee0f9815a0e24eefc0fe20
Raghav <70934347+raggzy271@users.noreply.github.com>
2022-08-04 14:13:04 +0530

    Fixed hrms routing issue

--- src/app/pages/admin/hrms/hrms-routing.module.ts
+++ src/app/pages/admin/hrms/hrms-routing.module.ts
@@ -6,23 +6,16 @@ const routes: Routes = [
   {
     path: '',
     component: HrmsComponent,
-    children: [
-      {
-        path: '',
-        redirectTo: 'dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'attendance',
-        loadChildren: () =>
-        import('./attendance/attendance.module').then(m => m.AttendanceModule)
-      }
-    ]
+  },
+  {
+    path: 'attendance',
+    loadChildren: () =>
+      import('./attendance/attendance.module').then((m) => m.AttendanceModule),
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class HrmsRoutingModule { }
+export class HrmsRoutingModule {}
