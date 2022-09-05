c84dfaac56548c691e5712f1431f296ee89b55c3
Brahma behera <beherabrahmananda77@gmail.com>
2022-09-06 00:51:44 +0530

    updated pos pannel

--- src/app/pages/pos/categories/categories.component.ts
+++ src/app/pages/pos/categories/categories.component.ts
@@ -74,9 +74,42 @@ export class CategoriesComponent implements OnInit {
       color: 'success'
     },
   ]
+items :any=[
+{
+  id: 1,
+  name: 'Vada Pav ',
+  image:'/assets/vada.svg',
+  price:333,
+  quantity:2,
+
+},
+{
+  id: 1,
+  name: 'Chapati Pav ',
+  image:'/assets/vada.svg',
+  price:100,
+  quantity:5,
+  
+},
+{
+  id: 1,
+  name: ' pakoda ',
+  image:'/assets/vada.svg',
+  price:20,
+  quantity:2,
+  
+}
+]
+
+total:any=0;
 
 
   ngOnInit(): void {
+    for(let i=0;i<this.items.length;i++){
+
+     this.total+=this.items[i].quantity*this.items[i].price
+    }
+    console.log(this.total)
   }
 
   widgets = [
